2c3632e5f610d1ba2ce13f9f3e95e90dabb67ac8
BaraAksayeth25 <mbara4321@gmail.com>
2020-10-29 12:17:32 +0700

    chore(test): not expect result

--- test/app.test.js
+++ test/app.test.js
@@ -4,6 +4,6 @@ const { greeting } = require('../user');
 describe('Test Middleware', () => {
   it('Greeting ', () => {
     const result = greeting('bara');
-    expect(result).to.be.eq('Hello wahyu, Welcome to My Job');
+    expect(result).to.be.eq('Hello wahyu dan dika, Welcome to My Job');
   });
 });
