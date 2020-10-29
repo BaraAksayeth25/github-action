5c7c51d90d101571b6b171cff2dd078d46096beb
BaraAksayeth25 <mbara4321@gmail.com>
2020-10-29 12:31:11 +0700

    feat(packages): add mocha and supertest

--- test/app.test.js
+++ test/app.test.js
@@ -1,9 +1,17 @@
+const request = require('supertest');
 const { expect } = require('chai');
 const { greeting } = require('../user');
+const app = require('../index');
 
-describe('Test Middleware', () => {
-  it('Greeting ', () => {
+describe('Test API', () => {
+  it('Middleware Greeting ', () => {
     const result = greeting('bara');
     expect(result).to.be.eq('Hello Bara, Welcome to My Job');
+  });
+
+  it('Request user', (done) => {
+    request(app)
+      .get('/user/dadang')
+      .expect('Hello Dadang, Welcome to My Job', done);
   });
 });
